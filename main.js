054fb1e65c19db3194295d3a7e9bf72472aaba98
Rohan kumar <75016222+Rudraksh42@users.noreply.github.com>
2022-04-08 15:30:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+song = ""
+
+function preload() {
+
+    song = loadSound("music.mp3");
+
+}
+
+function setup() {
+
+    canvas = createCanvas(600, 600);
+    canvas.center()
+
+    video = createVideo(VIDEO);
+    video.hide();
+}
+
+function draw() {
+
+    image(video, 0, 0, 600, 600);
+}
+
+function playMusic() {
+
+    song.play();
+}
